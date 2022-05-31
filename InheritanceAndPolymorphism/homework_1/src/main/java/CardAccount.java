b3d758c0ff3ee485d6c255f37a3b4745f9d61d6f
Aleksei <a.borisech@yandex.ru>
2022-05-31 12:10:00 +0500

    the task is done

--- InheritanceAndPolymorphism/homework_1/src/main/java/CardAccount.java
+++ InheritanceAndPolymorphism/homework_1/src/main/java/CardAccount.java
@@ -1,9 +1,8 @@
 public class CardAccount extends BankAccount {
 
-    private final double cashWithdrawalFee = 0.01;
-
     @Override
     public void take(double amountToTake) {
+        double cashWithdrawalFee = 0.01;
         super.take(amountToTake + amountToTake * cashWithdrawalFee);
     }
 }
