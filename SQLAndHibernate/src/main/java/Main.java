c4aac9ea506abfa86740ef091eb3df00ebdbb093
Aleksei <a.borisech@yandex.ru>
2022-10-15 15:39:25 +0500

    the task is done

--- SQLAndHibernate/src/main/java/Main.java
+++ SQLAndHibernate/src/main/java/Main.java
@@ -5,6 +5,8 @@ import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -17,8 +19,22 @@ public class Main {
 
         Session session = sessionFactory.openSession();
 
-            Course course = session.get(Course.class, 10);
-            System.out.println(course.getName() + " " + course.getStudentsCount());
+            Course course = session.get(Course.class, 1);
+            List<Student> studentList = course.getStudents();
+            //studentList.forEach(student -> System.out.println(student.getName()));
+
+            Subscription subscription = session.get(Subscription.class, new SubscriptionKey(55,33));
+            //System.out.println(subscription.getSubscriptionDate());
+
+            Teacher teacher = session.get(Teacher.class, 1);
+            //System.out.println(teacher.getName());
+
+            Student student = session.get(Student.class, 10);
+            //System.out.println(student.getName() + " " + student.getAge());
+
+            PurchaseList purchaseList = session.get(PurchaseList.class, new PurchaseKey("Бойков Максим","Веб-разработчик c 0 до PRO"));
+            System.out.println(purchaseList.getStudentName() + " " + purchaseList.getPrice()
+                    + " " + purchaseList.getSubscriptionDate());
 
         sessionFactory.close();
     }
